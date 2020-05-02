7fd4beb998d905cd96423c1b345ee7eafa096372
Anna <anna.scibska@campusid.eu>
2020-05-02 14:27:20 +0200

    comment model updated

--- models/Comment.js
+++ models/Comment.js
@@ -1,52 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const CommentSchema = new Schema({
     user: {
+        type: Schema.Types.ObjectId
+    },
+    book: {
         type: Schema.Types.ObjectId
     },
     text: {
         type: String,
         required: true
     },
-    name: {
+    mark: {
         type: String
     },
-    avatar: {
-        type: String
-    },
-    likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId
-            }
-        }
-    ],
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
     date: {
         type: Date,
         default: Date.now
     }
 });
 
-module.exports = mongoose.model('post', PostSchema);
+module.exports = mongoose.model('comment', CommentSchema);

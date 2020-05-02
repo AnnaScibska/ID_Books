1534ed4048828fde9f10818daf81871bad64dba0
Anna <anna.scibska@campusid.eu>
2020-05-02 10:59:27 +0200

    db connected

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 // const path = require('path');
 
 const app = express();
@@ -11,8 +11,8 @@ app.get('/', (req, res) => res.send('API Running'))
 
 
 
-// // Connect Database
-// connectDB();
+// Connect Database
+connectDB();
 
 // // Init Middleware
 // app.use(express.json());

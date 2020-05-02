3b43d292867af439c9f8b55b76686cafb9192d14
Anna <anna.scibska@campusid.eu>
2020-05-02 13:47:30 +0200

    models added

---
+++ models/Book.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    year: {
+        type: String
+    },
+    imageUrl: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('book', BookSchema);
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    books: [
+        {
+            book: {
+                type: Schema.Types.ObjectId
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
